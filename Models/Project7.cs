ca4ed90b18194b4a3e1c2ba458f7dac9bab4d6ba
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-09-27 10:36:04 -0700

    trying to add method for calculating totol price

--- Models/Project7.cs
+++ Models/Project7.cs
@@ -8,34 +8,35 @@ namespace Bakery
         public int Price { get; set; }
         public int Quantity { get; set; }
 
-        public Bread(int Quantity, int TotalPrice) {
+        public Bread(int Quantity, int Price) {
             Price = 0;
             Quantity = 0;
-            TotalPrice = 0;
+    
 
         }
 
-        public void DecidePrice(int getActualQuantity)  {
+        public void DecidePrice(int getActualQuantity, int getTotalPrice)  {
             AddPrice();
             AddQuantity(getActualQuantity);
-            ExitPrice(TotalPrice);
-
+            AddTotalPrice(getTotalPrice);
+            
 
         }
 
        public void AddPrice()
        {
-          Price += 5;
+          Price = Price + 5;
        }
 
        public void AddQuantity(int getActualQuantity) {
             Quantity += getActualQuantity;
+            
        }
-        
-       public void ExitPrice(int TotalPrice) 
-       {
-           TotalPrice = Price * Quantity;
+
+       public int AddTotalPrice(int getTotalPrice) {
+          return Price * getActualQuantity;
        }
+       
     }
     
 }
