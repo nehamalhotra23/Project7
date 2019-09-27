0b03f4ffc841e3ea57e2307f84dc6fa414e4511c
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-09-27 11:27:05 -0700

    add condition to choose between bread and pastery

--- Models/Program.cs
+++ Models/Program.cs
@@ -5,19 +5,35 @@ class Program
 {
     static void Main()
     {  
-
+       Console.WriteLine("WELCOME TO TASTY BAKERY");
+       Console.WriteLine("--------------------");
        Console.WriteLine("Price of Single Loaf of bread is $5 and Pastery is $2");
+        Console.WriteLine("--------------------");
        Console.WriteLine("Offer Price: Buy 2 Get 1 Bread free and 'Buy 3 Pastery for $5' ");
-       string OrderQuantity =  Console.ReadLine(); 
-       int OrderNumber = int.Parse(OrderQuantity);
-       Bread newBread = new Bread(OrderNumber);
-   
-       if (OrderNumber == 2 || OrderNumber == 1) {
+       Console.WriteLine("--------------------");
+       Console.WriteLine("What's your order? BREAD or Pastery");
+       string UserInput = Console.ReadLine();
+        
+    //  Bread newBread = new Bread(OrderNumber);
+
+       if (UserInput == "Bread" || UserInput == "bread") {
+            Console.WriteLine("How many Loaf of bread you want?");
+            string OrderQuantity =  Console.ReadLine();
+            int OrderNumber = int.Parse(OrderQuantity);
+         
+         if (OrderNumber == 2 || OrderNumber == 1) {
            Console.WriteLine("$5");
-       } else if (OrderNumber > 2) {
-         int outPut = newBread.DecidePrice(OrderNumber);
-         Console.WriteLine(outPut);
+        }
+            
+       } else {
+            Console.WriteLine("Pastery");
        }
+
+       
+    //     else if (OrderNumber > 2) {
+    //     newBread.DecidePrice(OrderNumber);
+    //      Console.WriteLine(outPut);
+    //    }
    
        }
     }
