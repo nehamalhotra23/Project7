747c3b3be8b6ed5f49bfe60e7c50150f4b9ff15b
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-09-27 11:43:47 -0700

    add Class for pastery and methods for price

--- Models/Program.cs
+++ Models/Program.cs
@@ -14,26 +14,28 @@ class Program
        Console.WriteLine("What's your order? BREAD or Pastery");
        string UserInput = Console.ReadLine();
         
-    //  Bread newBread = new Bread(OrderNumber);
-
        if (UserInput == "Bread" || UserInput == "bread") {
-            Console.WriteLine("How many Loaf of bread you want?");
-            string OrderQuantity =  Console.ReadLine();
-            int OrderNumber = int.Parse(OrderQuantity);
+        Console.WriteLine("How many Loaf of bread you want?");
+        string OrderQuantity =  Console.ReadLine();
+        int OrderNumber = int.Parse(OrderQuantity);
+        Bread newBread = new Bread(OrderNumber);
          
          if (OrderNumber == 2 || OrderNumber == 1) {
            Console.WriteLine("$5");
+        } else {
+           newBread.DecidePrice(OrderNumber);
         }
             
-       } else {
-            Console.WriteLine("Pastery");
+       } else if (UserInput == "Pastery" || UserInput == "pastery") {
+            Console.WriteLine("How many pasteries you want?");
+            string OrderQuantityTwo =  Console.ReadLine();
+            int OrderNumberPastery = int.Parse(OrderQuantityTwo);
+
+
        }
 
        
-    //     else if (OrderNumber > 2) {
-    //     newBread.DecidePrice(OrderNumber);
-    //      Console.WriteLine(outPut);
-    //    }
+    
    
        }
     }
--- Models/Project7.cs
+++ Models/Project7.cs
@@ -12,7 +12,6 @@ namespace Bakery
             Price = 5;
             Quantity = 0;
     
-
         }
 
         public void  DecidePrice(int getActualQuantity)  {
@@ -33,5 +32,38 @@ namespace Bakery
        }
        
     }
+
+    public class Pastery {
+
+        public int PasteryPrice { get; set; }
+        public int PasteryQuantity { get; set; }
+
+        public Pastery(int PasteryQuantity) {
+            PasteryPrice = 2;
+            PasteryQuantity = 0;
     
-}
+
+        }
+
+        public void  DecidePasteryPrice(int getActualQuantityPas)  {
+            
+            AddQuantityOfPastery(getActualQuantityPas);
+            AddTotalPasteryPrice(PasteryPrice);
+            
+
+        }
+
+       public void AddQuantityOfPastery(int getActualQuantityPas) {
+            PasteryQuantity += getActualQuantityPas;
+            
+       }
+
+       public void AddTotalPasteryPrice(int pasteryPrice) {
+           pasteryPrice = PasteryPrice * PasteryQuantity;
+       }
+       
+    }
+
+
+    }
+    
