417bdfa64ccf438dd8ab7071a0e97916f363f41f
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-09-29 13:45:40 -0700

    add functionality to decide price if bread order quantity is less than 2

--- Models/Program.cs
+++ Models/Program.cs
@@ -7,7 +7,7 @@ class Program
        Console.WriteLine("WELCOME TO TASTY BAKERY");
        Console.WriteLine("--------------------");
        Console.WriteLine("Price of Single Loaf of bread is $5 and Pastery is $2");
-        Console.WriteLine("--------------------");
+       Console.WriteLine("--------------------");
        Console.WriteLine("Offer Price: Buy 2 Get 1 Bread free");
        Console.WriteLine("--------------------");
        Console.WriteLine("How many loafs of bread you want?");
@@ -20,9 +20,13 @@ class Program
         string OrderQuantityTwo =  Console.ReadLine();
         int OrderNumberPastery = int.Parse(OrderQuantityTwo);
         Pastery newPastery = new Pastery(OrderNumberPastery);
-
         int totalPricePastery = newPastery.DecidePasteryPrice(OrderNumberPastery * 2);
-        
-        Console.WriteLine("Total Order Price is " + newBread.DecidePrice(OrderNumber * 5 * 74 / 100 + totalPricePastery));
+        int totalPricebread = newBread.DecidePrice(OrderNumber * 5 * 75 / 100 + 1);
+
+        if (OrderNumber < 2 ){
+          Console.WriteLine("Total order price is " + (5 + totalPricePastery));
+        } else if (OrderNumber > 2) {
+         Console.WriteLine("Total Order Price is " +  (totalPricebread + totalPricePastery));
+        } 
     }
 }
